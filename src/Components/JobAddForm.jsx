6c680d2682a61963f8c5f84f3045554a1d0ae3e4
amirah <mirez.data@gmail.com>
2024-03-17 10:59:52 +0800

    commit changes

--- src/Components/JobAddForm.jsx
+++ src/Components/JobAddForm.jsx
@@ -4,23 +4,35 @@ function JobForm(){
     return(
         <div className="w-full py-16 px-6" id="job_form">
             <div>
-                <h2 className="text-center text-2xl my-4">Adding Job Application</h2>
+                <h2 className="text-center text-3xl my-4">Adding Job Application</h2>
             </div>
             <div >
-                <form className="flex flex-col p-6">
+                <form className="flex flex-col p-6 text-lg border-4 border-sky-300">
                     <label className="" for="job-title">Job Title:</label>
-                    <input className="mb-4 w-[500px]" type="text" id="job-title" name="job-title" required />
+                    <input className="mb-4 w-[500px] rounded-md px-2" type="text" id="job-title" name="job-title" required />
                     
                     <label for="job-title">Company:</label>
-                    <input className="mb-4 w-[500px]" type="text" id="company" name="company" required />
+                    <input className="mb-4 w-[500px] rounded-md text-md px-2" type="text" id="company" name="company" required />
 
-                    <label for="job-title">Category:</label>
-                    <input className="mb-4 left-0" type="checkbox" id="goverment" name="category" value="Goverment" required /> 
-                    <label for="goverment">Goverment</label><br />
-                    <input className="mb-4 left-0" type="checkbox" id="swasta" name="category" value="Swasta" required />
-                    <label for="swasta">Swasta</label><br />
+                    <label for="job-category">Category:</label>
+                    <select className="mb-4 w-[250px] rounded-md text-md px-2" name="job-category" id="category">
+                        <option value="goverment">Gorvenment</option>
+                        <option value="swasta">Swasta</option>
+                        <option value="freelance">Freelance</option>
+                    </select>
+
+                    <label for="job-portal">Job Portal:</label>
+                    <select className="mb-4 w-[250px] rounded-md text-md px-2" name="job-portal" id="portal">
+                        <option value="jobstreet">Jobstreet</option>
+                        <option value="futurejob">myFutureJob</option>
+                        <option value="indeed">Indeed</option>
+                        <option value="linkedin">LinkedIn</option>
+                        <option value="upwork">Upwork</option>
+                        <option value="fiver">Fiver</option>
+                        <option value="others">Others</option>
+                    </select>
                     
-                    <label for="job-title">Date Apply:</label>
+                    <label for="date">Date Apply:</label>
                     <input className="mb-4 w-[250px]" type="date" id="date-apply" name="date-apply" required />
 
 
